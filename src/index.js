38efc1f830d65ae3db18514063fe9e8f5285610d
Jack Pritchard <jackdavieswork@hotmail.co.uk>
2019-12-07 21:29:19 +0000

    Set to GET

--- src/index.js
+++ src/index.js
@@ -6,45 +6,29 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const ogs = require("open-graph-scraper");
-
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is a basic express app to return Open Graph meta information");
-});
-
-app.post("/", async (req, res) => {
-  const { url } = req.body;
+  const url = req.param("url");
 
   if (!url) {
-    res.sendStatus(400);
-    throw Error("No URL provided");
+    res.status(400).send("No URL provided");
   }
 
   const options = {
     url,
-    timeout: 20000
+    timeout: process.env.TIMEOUT || 20000
   };
 
   ogs(options, (err, res) => {
     console.log("error:", err); // This is returns true or false. True if there was a error. The error it self is inside the results object.
     console.log("results:", res);
 
-    if (!err) {
-      res.sendStatus(400);
-      throw Error(err);
-    }
-
-    if (!err && res) {
-      res.sendStatus(200);
-      res.send(res);
-    }
+    if (err) res.status(400).send(err);
+    if (!err && res) res.status(200).send(res);
   });
 });
 
