09f6d5f996d3ce1ce040a5bba0907ac83d8cbbf4
Jack Pritchard <jack@noface.co.uk>
2019-12-08 08:41:49 +0000

    Fix application

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
 const ogs = require("open-graph-scraper");
+
+dotenv.config();
+const app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  const url = req.param("url");
+  const { url } = req.query;
+
+  console.log(req.query)
 
   if (!url) {
-    res.status(400).send("No URL provided");
+    res.send("No URL provided");
   }
 
   const options = {
@@ -23,12 +26,12 @@ app.get("/", (req, res) => {
     timeout: process.env.TIMEOUT || 20000
   };
 
-  ogs(options, (err, res) => {
+  ogs(options, (err, result) => {
     console.log("error:", err); // This is returns true or false. True if there was a error. The error it self is inside the results object.
-    console.log("results:", res);
+    console.log("results:", result);
 
-    if (err) res.status(400).send(err);
-    if (!err && res) res.status(200).send(res);
+    if (err) res.send(result.error);
+    if (!err && result) res.send(result);
   });
 });
 
